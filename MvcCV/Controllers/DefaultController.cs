9e3fe2bb91dd1a825e39c5f778cb8c7fe0696a97
kaan koc <kaankoc01@gmail.com>
2024-07-09 03:58:47 +0300

    Add project files.

---
+++ MvcCV/Controllers/DefaultController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCV.Models.Entity;
+
+namespace MvcCV.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = db.TblEgitimlerim.ToList();
+            return PartialView(egitimler);
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.TblYeteneklerim.ToList();
+            return PartialView(yetenekler);
+        }
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TblHobilerim.ToList();
+            return PartialView(hobiler);
+        }
+        public PartialViewResult Sertfikalarım()
+        {
+            var sertfika = db.TblSertfikalarim.ToList();
+            return PartialView(sertfika);
+        }
+        [HttpGet]
+        public PartialViewResult iletisim()
+        {
+            var iletisim = db.TblIletisim.ToList();
+            return PartialView(iletisim);
+        }
+        [HttpPost]
+        public PartialViewResult iletisim(TblIletisim t)
+        {
+            t.Tarih =DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.TblIletisim.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+    }
+}
